477171edd6b3ccdfb992f83faa4151b44932ab55
fkerbey <nsaidou19744r@126.com>
2021-04-23 15:59:30 +0800

    test

--- src/main/java/com/w/springboot/Test.java
+++ src/main/java/com/w/springboot/Test.java
@@ -3,6 +3,17 @@ package com.w.springboot;
 public class Test {
 
 	public static void main(String[] args) {
+		print();
+	}
+	
+	public static void print() {
+		System.out.println("aaaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaaa");
 		System.out.println("aaaaaaaaaaaaaaaaaa");
 	}
 }
